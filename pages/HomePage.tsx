535eb86d48c2971bc986486f1df6ed3b9da0a8a2
Naimlyss <123730481+dotNaimlyss@users.noreply.github.com>
2024-02-17 01:06:24 +0800

    Update HomePage.tsx

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -47,7 +47,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="relative">
       <h1 className="text-2xl font-semibold text-gray-700 mb-4">
-        Song Tracks you may like!
+        Song Tracks you may like!!
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {recommendedSongs.map((recommendation, index) => (
