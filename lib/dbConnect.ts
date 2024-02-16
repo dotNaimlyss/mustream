b3b3623987aadbc7d4bc0031ea4b4ffd0306fa50
Naimlyss <123730481+dotNaimlyss@users.noreply.github.com>
2024-02-17 04:39:32 +0800

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -12,30 +12,39 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  // Check the initial connection state
-  console.log(`Initial connection state: ${mongoose.connection.readyState}`);
+// async function dbConnect() {
+//   // Check the initial connection state
+//   console.log(`Initial connection state: ${mongoose.connection.readyState}`);
 
-  if (cached.conn) {
-    console.log(`Using cached database connection`);
-    return cached.conn;
-  }
+//   if (cached.conn) {
+//     console.log(`Using cached database connection`);
+//     return cached.conn;
+//   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      console.log("Database connected");
-      return mongoose;
-    }).catch((error) => {
-      console.error('Database connection error:', error);
-      throw error;
-    });
-  }
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+//       console.log("Database connected");
+//       return mongoose;
+//     }).catch((error) => {
+//       console.error('Database connection error:', error);
+//       throw error;
+//     });
+//   }
 
-  cached.conn = await cached.promise;
+//   cached.conn = await cached.promise;
   
-  // Check the connection state after attempting to connect
-  console.log(`Connection state after connect: ${mongoose.connection.readyState}`);
-  return cached.conn;
+//   // Check the connection state after attempting to connect
+//   console.log(`Connection state after connect: ${mongoose.connection.readyState}`);
+//   return cached.conn;
+// }
+
+async function dbConnect() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
 }
 
 export default dbConnect;
