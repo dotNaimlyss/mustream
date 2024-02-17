86079e2acc4409c8095f816adbb6ac753761655a
Naimlyss <123730481+dotNaimlyss@users.noreply.github.com>
2024-02-17 13:07:31 +0800

    Update chatComponent.tsx

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -25,7 +25,7 @@ const ChatComponent: React.FC<ChatComponentProps> = ({
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
-    socketRef.current = io("http://localhost:3000");
+    socketRef.current = io("https://mustream.vercel.app:3000");
 
     socketRef.current.emit("join session", selectedTrack, selectedArtist);
     socketRef.current.on("chat message", (msg: ChatMessage) => {
