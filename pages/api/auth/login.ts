b97d544fb0a5b33405c587b741d0727846e17180
Naimlyss <123730481+dotNaimlyss@users.noreply.github.com>
2024-02-18 19:11:34 +0800

    Update login.ts

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -28,9 +28,9 @@ export default async function handler(req, res) {
           {
             expiresIn: "1h", // token will expire in 1 hour
           }
-          console.log(user)
-        );
 
+        );
+        console.log(user)
         // Return user and token
         res.status(200).json({ token });
       } else {
