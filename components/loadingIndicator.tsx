50d387ccba5e52b4981c2ab7bbfe673271e579d3
Naimlyss <123730481+dotNaimlyss@users.noreply.github.com>
2024-02-19 04:42:18 +0800

    Update loadingIndicator.tsx

--- components/loadingIndicator.tsx
+++ components/loadingIndicator.tsx
@@ -19,7 +19,7 @@ export const LoadingIndicator = () => {
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
       <div className="w-16 h-16 bg-primary rounded-full flex items-center justify-center">
-        <div className="text-white text-2xl">Loading {loadingChar}</div>
+        <div className="text-white text-2xl"> {loadingChar}</div>
       </div>
     </div>
   );
